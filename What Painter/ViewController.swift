f7b275935465e8360d6c2a69c3dabcfca75cc6ff
denchiklut <denchiklut@gmail.com>
2020-02-29 00:08:47 +0300

    Initial Commit

---
+++ What Painter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  What Painter
+//
+//  Created by Александров Денис Александрович on 29.02.2020.
+//  Copyright © 2020 Александров Денис Александрович. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
