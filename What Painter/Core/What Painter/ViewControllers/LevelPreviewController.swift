1b2c40a9fd7a705f89f7a439febedc208748844f
denchiklut <denchiklut@gmail.com>
2020-03-16 10:23:53 +0300

    WIP

--- What Painter/Core/What Painter/ViewControllers/LevelPreviewController.swift
+++ What Painter/Core/What Painter/ViewControllers/LevelPreviewController.swift
@@ -19,16 +19,6 @@ class LevelPreviewController: UIViewController {
         self.modalTransitionStyle = UIModalTransitionStyle.crossDissolve
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        self.tabBarController?.tabBar.isHidden = true
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        self.tabBarController?.tabBar.isHidden = false
-    }
-    
     let levelImage: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFill
